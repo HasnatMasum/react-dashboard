6b1fbe52ab4026fcd174d9856f3d78a70d7e3ff6
hasnat <themespassionh@gmail.com>
2023-04-02 17:33:10 +0600

    subtite component setup

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,6 +1,24 @@
+import { Card, Space,Col, Row, Statistic } from 'antd';
+import Subtitle from '../components/Subtitle';
+
 const Dashboard = ()=>{
     return(
-        <div>Dashboard</div>
+        <>
+       <Subtitle text="Dashboard" />
+        <Row gutter={16}>
+        
+    <Col span={8}>
+    <Card>
+      <Statistic title="Feedback" value={1128} prefix="hello" />
+      </Card>
+    </Col>
+    <Col span={8}>
+    <Card>
+      <Statistic title="Unmerged" value={93} suffix="/ 100" />
+      </Card>
+    </Col>
+  </Row>
+  </>
     )
 }
 
